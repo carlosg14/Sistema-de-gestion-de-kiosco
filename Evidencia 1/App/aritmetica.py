b7f9cdab391fdcceca43b48d06246d5e56bd5fda
Felix Tapia <felixcruztapia@gmail.com>
2024-09-02 18:12:37 -0300

    Modulo aritmetica.py

---
+++ Evidencia 1/App/aritmetica.py
@@ -0,0 +1,28 @@
+def sumar(a: float, b: float):
+    return round(a + b, 2)
+
+def restar(a: float, b: float):
+    return round(a - b, 2)
+
+def dividir(a: float, b: float):
+    try:
+        return round(a / b, 2)
+    except ZeroDivisionError:
+        print('No se puede dividir por cero..')
+
+def multiplicar(a: float, b: float):
+    return round(a * b, 2)
+
+def sumar_n(*numeros: float):
+    suma = 0
+    for numero in numeros:
+        suma = sumar(suma, numero)
+    return round(suma, 2)
+
+
+def promedio_n(*numeros: float):
+    suma = sumar_n(*numeros)
+    return round(dividir(suma, len(numeros)), 2)
+
+
+
