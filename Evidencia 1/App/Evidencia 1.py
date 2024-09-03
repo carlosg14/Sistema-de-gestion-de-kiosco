9426ac915b539723899dcb20265704bfa8620d21
Felix Tapia <felixcruztapia@gmail.com>
2024-09-02 22:14:35 -0300

    Uso de excepciones

--- Evidencia 1/App/Evidencia 1.py
+++ Evidencia 1/App/Evidencia 1.py
@@ -1,73 +1,91 @@
 
 
-def login(credenciales: list[dict]):
+def login(credenciales: dict):
     """
     Se solicitan y validan las credenciales necesarias para ingresar al sistema.
 
     Parametros
-        credenciales [list] : Credenciales necesarias para comprobar la identidad del usuario.
+        credenciales [dict] : Credenciales necesarias para comprobar la identidad del usuario.
 
     """
     intentos = 0
     while intentos < 4:
 
         # Se solicitan los datos.
+
         print('=' * 20)
+
         nombre_usuario = input('Ingrese su nombre de usuario: ')
-        contraseña = int(input('Ingrese su contraseña: '))
+        contraseña = input('Ingrese su contraseña: ')
+
         print('=' * 20)
         print('\n')
 
         # Se comprueba su validez.
-        for usuario in credenciales:
-            if usuario['nombre'] == nombre_usuario and usuario['contraseña'] == contraseña:
+
+        try:
+            if credenciales[nombre_usuario]['contraseña'] == contraseña:
                 print('\nSesion iniciada correctamente....')
-                intentos = 5
                 break
-            elif usuario['nombre'] == nombre_usuario and usuario['contraseña'] != contraseña:
+            else:
                 intentos += 1
                 print('\033[1;31m ')  # Colorea de rojo las letras de la consola
-                print(f'Contraseña incorrecto.... Te quedan {intentos} intentos.')
+                print(f'Contraseña incorrecta.... Te quedan {4 - intentos} intentos.')
                 print('\033[0;57m ')  # Vuelve al color blanco
-                break
-        else:
+        except KeyError:
             print('\033[1;31m ')  # Colorea de rojo las letras de la consola
             print(f'Usuario incorrecto....')
             print('\033[0;57m ')  # Vuelve al color blanco
             print('\n')
 
 
-def nuevo_usuario(credenciales: list[dict]):
+def nuevo_usuario(credenciales: dict):
     """
     Se solicitan y verifican los datos necesarios para registrar un usuario. Si todos los datos
     son correctos se agrega un nuevo diccionario a la lista credenciales.
 
     """
+    # Se solicitan los datos del nuevo usuario
+
+    dni = input('Ingrese su Dni: ')
+    nombre_real = input('Ingrese su nombre: ')
+    apellido = input('Ingrese su apellido: ')
+    correo = input('Ingrese su correo electronico: ')
+    fecha_nac = input('Ingrese su fecha de nacimiento: ')
+
+    # Se verifica que el nombre de usuario cumpla las condiciones.
     while True:
         print('=' * 20)
-        nombre = input('Ingrese su nombre de usuario ( Entre 6 a 12 caracteres): ')
+        nombre_usuario = input('Ingrese su nombre de usuario ( Entre 6 a 12 caracteres): ')
 
         # Se verifica si la longitud del nombre es correcta
-        if len(nombre) > 12 or len(nombre) < 6:
+        if len(nombre_usuario) > 12 or len(nombre_usuario) < 6:
             print('\033[1;31m ')  # Colorea de rojo las letras de la consola
             print(f'El nombre de usuario debe tener entre 6 a 12 caracteres')
             print('\033[0;57m ')  # Vuelve al color blanco
             continue
 
         # Se verifica si el nombre de usuario es unico
-        existe = False
-        for usuario in credenciales:
-            if usuario['nombre'] == nombre:
-                print('\033[1;31m ')  # Colorea de rojo las letras de la consola
-                print(f'El nombre de usuario ya existe.')
-                print('\033[0;57m ')  # Vuelve al color blanco
-                existe = True
-                break
-        if existe:
+        if nombre_usuario in credenciales.keys():
+            print('\033[1;31m ')  # Colorea de rojo las letras de la consola
+            print(f'El nombre de usuario ya existe.')
+            print('\033[0;57m ')  # Vuelve al color blanco
             continue
+        else:
+            break
 
+    # Se verifica que la contraseña cumpla las condiciones solicitadas.
     while True:
-        contraseña = input('Ingrese su contraseña ( Debe contener minimo 8 caracteres):: ')
+
+        print('\033[1;31m ')  # Colorea de rojo las letras de la consola
+        print(f'La contraseña debe cumplir al menos dos de las siguientes condiciones: '
+              f'\n- Al menos un numero.'
+              f'\n- Al menos una letra mayuscula.'
+              f'\n- Al menos una letra minuscula.'
+              f'\n- Al menos un caracter especial.')
+        print('\033[0;57m ')  # Vuelve al color blanco
+
+        contraseña = input('Ingrese su contraseña ( Debe contener minimo 8 caracteres): ')
 
         # Se verifica si la contraseña tiene la longitud correcta
         if len(contraseña) < 8:
@@ -76,41 +94,36 @@ def nuevo_usuario(credenciales: list[dict]):
             print('\033[0;57m ')  # Vuelve al color blanco
             continue
 
-        condiciones = 0
+        if contraseña.isupper() and contraseña.isalpha():
+            print('Contraseña invalida. Verifique si cumple los requisitos.')
+            print('=' * 20)
+            print('\n')
+            continue
+        elif contraseña.islower() and contraseña.isalpha():
+            print('Contraseña invalida. Verifique si cumple los requisitos.')
+            print('=' * 20)
+            print('\n')
+            continue
+        elif contraseña.isnumeric():
+            print('Contraseña invalida. Verifique si cumple los requisitos.')
+            print('=' * 20)
+            print('\n')
+            continue
+        elif not (any(car.isdigit() for car in contraseña)):
+            print('Contraseña invalida. Verifique si cumple los requisitos.')
+            print('=' * 20)
+            print('\n')
+            continue
+        else:
+            break
 
-        # Se comprueba que tenga algun numero
-        if contraseña.isalnum():
-            condiciones += 1
+    credenciales[nombre_usuario] = {'nombre_real': nombre_real,
+                                    'contraseña': contraseña,
+                                    'apellido': apellido,
+                                    'correo': correo,
+                                    'dni': dni,
+                                    'fecha_nac': fecha_nac}
 
-        # Se verifica si tiene al menos una mayuscula
-        if contraseña.isupper():
-            condiciones += 1
-        # Se verifica si tiene al menos una minuscula
-        elif contraseña.islower():
-            condiciones += 1
-        # Verdadero si tiene mayusculas y minusculas
-        elif not contraseña.isnumeric():
-            condiciones += 2
-
-        if condiciones < 2:
-            print('\033[1;31m ')  # Colorea de rojo las letras de la consola
-            print(f'La contraseña debe cumplir al menos dos de las siguientes condiciones: '
-                  f'- Al menos un numero.'
-                  f'- Al menos una letra mayuscula.'
-                  f'- Al menos una letra minuscula.'
-                  f'- Al menos un caracter especial.')
-            print('\033[0;57m ')  # Vuelve al color blanco
-
-        print('=' * 20)
-        print('\n')
-
-    dni = input('Ingrese su Dni: ')
-    nombre_real = input('Ingrese su nombre: ')
-    apellido = input('Ingrese su apellido: ')
-    correo = input('Ingrese su correo electronico: ')
-    fecha_nac = input('Ingrese su fecha de nacimiento: ')
-
-    credenciales.append({'nombre': nombre, 'contraseña': contraseña})
     return credenciales
 
 
@@ -119,7 +132,7 @@ def nueva_contra():
 
 
 def menu():
-    credenciales = [{'nombre': 'admin', 'contraseña': 1234}]
+    credenciales = {'admin': {'nombre_real': 'admin', 'contraseña': '1234', 'apellido': 'x'}}
 
     n = '\nSeleccione una opcion: '
 
