25f20fc0565fb4371da984fa9190caa3784ce816
Felix Tapia <felixcruztapia@gmail.com>
2024-10-20 21:30:27 -0300

    Mejoras en la BD

---
+++ Evidencia 3/Base de datos/config.py
@@ -0,0 +1,9 @@
+"""
+Este es un archivo de configuracion. Aqui debe cargar las credenciales de la base de datos.
+"""
+
+
+config = {'host': 'localhost',
+          'user': 'root',
+          'password': '',
+          'database': 'sistema_de_gestion_kiosco'}
